41067d918ff3b2d016340006552bb0cc36dc2657
admin <admin>
2017-02-08 15:12:11 +0800

    a

--- src/servers/master/master.go
+++ src/servers/master/master.go
@@ -5,31 +5,39 @@ import (
 	"projectx/src/config"
 	"projectx/src/components/tcpserver"
 	"fmt"
+	"net"
+	"projectx/src/util"
+	"projectx/src/protocol"
 )
 
-type Master interface {
+type GateWay interface {
 	Start()
 	Stop()
 }
 
 type master struct {
-	q *queue.QueueRouter
-	c config.MasterConfig
-	server tcpserver.Server
+	chs       *queue.MultiChans
+	c         config.MasterConfig
+	server    tcpserver.Server
+	backend   chan *protocol.Message
 }
 
-func NewMaster() *master {
-	master := new(master)
-	master.c.Name = "master"
-	master.c.Host = "127.0.1.0:9910"
-	master.q = queue.NewMessageRouter()
-	master.server = tcpserver.NewServer(master.q)
-	return master
+func NewGateWay() *master {
+	gw := new(master)
+	gw.c.Name = "master"
+	gw.c.Host = "127.0.1.0:9910"
+	gw.chs = queue.NewMultiChans()
+	gw.server = tcpserver.NewServer(gw.chs)
+	return gw
 }
 
 func (r *master) Start() {
-	r.makerouter()
-	r.server.Start(r.c.TcpServerConfig)
+	r.connectserver()
+	r.server.Start(r.c.TcpServerConfig, &tcpserver.ClientEventCallback{
+		OnConnect: func(id int32) { r.OnConnect(id)},
+		OnClose: func(id int32) {r.OnClose(id)},
+		OnError: func(id int32) {r.OnError(id)},
+	})
 	r.run()
 }
 
@@ -38,26 +46,36 @@ func (r *master) Stop() {
 }
 
 func (r *master) run() {
-	for {
-		r.handlecmd()
-		r.handleio()
-		r.handletimer()
-	}
 	fmt.Println("master run finish")
 }
 
-func (r *master) makerouter() {
+func (r *master) registerclientrouter(id int32, size int32, serconn net.Conn) {
+	ch := r.chs.CreateChs(id, size)
+	go func() {
+		select {
+		case msg := <- ch:
+			util.WritePacket(serconn, msg, 1)
+		}
+	}()
+}
+
+func (r *master) OnConnect(id int32) {
 
 }
 
-func (r *master) handleio() {
+func (r *master) OnClose(id int32) {
 
 }
 
-func (r *master) handletimer() {
+func (r *master) OnError(id int32) {
 
 }
 
-func (r *master) handlecmd() {
-
+func (r *master) handelbackend() {
+	for {
+		select {
+		case msg := <- r.backend:
+			msg
+		}
+	}
 }
